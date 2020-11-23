fab820fe8caf71220c86cf43ae1346ef4ce92a73
leyden recinos <leyden.recinos@gmail.com>
2020-11-23 00:23:41 -0800

    Created a helper function to handle leap years

--- task.py
+++ task.py
@@ -1,2 +1,5 @@
-def my_func():
-    return "Hello World"
+def leap_yr(year):
+    """Helper func: Returns true if it's a leap year and False if it's not"""
+
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
