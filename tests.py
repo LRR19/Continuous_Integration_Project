b566ebacef5cb95b3d0bf60b554119a4cc31fca1
leyden recinos <leyden.recinos@gmail.com>
2020-11-23 00:24:06 -0800

    Created a test for the leap_yr func

--- tests.py
+++ tests.py
@@ -1,10 +1,12 @@
 import unittest
+from task import leap_yr
 
 
 class TestCase(unittest.TestCase):
 
-    def test1(self):
-        self.assertTrue(True)
+    """Unit tests for function 2 - def my_datetime(num_sec)"""
+    def test_leap_yr_func(self):
+        self.assertTrue(leap_yr(2000))
 
 
 if __name__ == '__main__':
