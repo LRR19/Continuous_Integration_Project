281ab6e89915923e9b68b45fb8690cc7abe5d478
leyden recinos <leyden.recinos@gmail.com>
2020-11-23 21:17:04 -0800

    created a random test: failed

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 import unittest
+import random
+from _datetime import datetime
 from task import leap_yr, my_datetime
 
 
@@ -13,6 +15,13 @@ class TestCase(unittest.TestCase):
 
     def test_secs1(self):
         self.assertTrue(my_datetime(1606186302))
+
+    def test_random(self):
+        for i in range(0, 1000):
+            num_secs = random.randint(0, 315537963048)
+            self.assertEqual(my_datetime(num_secs),
+                             datetime.utcfromtimestamp(num_secs).
+                             strftime('%m-%d-%Y'))
 
 
 if __name__ == '__main__':
