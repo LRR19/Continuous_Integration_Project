6e0f811bfffb939dd2d1c7568ea2db2171897302
leyden recinos <leyden.recinos@gmail.com>
2020-11-23 19:38:18 -0800

    created 2 tests to test the epoch date and current date.

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
-from task import leap_yr
+from datetime import datetime
+from task import leap_yr, my_datetime
 
 
 class TestCase(unittest.TestCase):
@@ -7,6 +8,12 @@ class TestCase(unittest.TestCase):
     """Unit tests for function 2 - def my_datetime(num_sec)"""
     def test_leap_yr_func(self):
         self.assertTrue(leap_yr(2000))
+
+    def test_yr_zero(self):
+        self.assertEqual(my_datetime(0), "01-01-1970")
+
+    def test_secs1(self):
+        self.assertTrue(my_datetime(1606186302))
 
 
 if __name__ == '__main__':
