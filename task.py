d6b7209fb9edd2650bdd0ac70cc72a076974da91
leyden recinos <leyden.recinos@gmail.com>
2020-11-27 21:22:22 -0800

    Fixed helper functions and things work now.

--- task.py
+++ task.py
@@ -1,13 +1,14 @@
 import math
+from datetime import datetime
 
-hour = 3600
-day = 86400
-week = 604800
-month = 2629743
 year = 31556926
+month = year / 12
+day = 86400
 epoc_year = 1970
 epoc_month = 1
 epoc_day = 1
+LEAP_MONTH_DAYS = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+REG_MONTH_DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 
 def leap_yr(lp_year):
@@ -15,42 +16,99 @@ def leap_yr(lp_year):
     return lp_year % 4 == 0 and (lp_year % 100 != 0 or lp_year % 400 == 0)
 
 
-def calc_year(secs) -> (int, float):
+def calc_year(secs) -> int:
     """Converts seconds to a year"""
     some_year = secs / year
     # take the number before the decimal
     a_year = some_year // 1
-    a_year_remainder = some_year % 1
-    return math.trunc(a_year), a_year_remainder
+    return math.trunc(a_year)
 
 
-def calc_month(secs) -> (int, float):
+def calc_month(secs) -> int:
     """Converts seconds to a month"""
-    y, y_remainder = calc_year(secs)
-    remaining_sec_in_year = y_remainder * year
-    month_from_remainder = remaining_sec_in_year / month
-    a_month = month_from_remainder // 1
-    a_month_remainder = month_from_remainder % 1
-    return math.trunc(a_month), a_month_remainder
+    y = calc_year(secs)
+    rem_secs = remain_secs_in_current_year(secs)
+    m = 0
+    if leap_yr(epoc_year + y):
+        for v in LEAP_MONTH_DAYS:
+            sec_in_month = v * day
+            if rem_secs - sec_in_month < 0:
+                break
+            else:
+                rem_secs -= sec_in_month
+                m += 1
+    else:
+        for v in REG_MONTH_DAYS:
+            sec_in_month = v * day
+            if rem_secs - sec_in_month < 0:
+                break
+            else:
+                rem_secs -= sec_in_month
+                m += 1
+    return m
 
 
-def calc_day(secs) -> (int, float):
+def remain_secs_in_current_year(secs) -> int:
+    i = calc_year(secs)
+    leap_month_years = 0
+    reg_month_years = 0
+    while i > 0:
+        if leap_yr(epoc_year + i):
+            leap_month_years += 1
+        else:
+            reg_month_years += 1
+        i -= 1
+
+    rem = secs - (((reg_month_years * 365) + (leap_month_years * 366)) *
+                  day)
+    return rem
+
+
+def calc_day(secs) -> int:
     """Converts seconds to a day"""
-    m, m_remainder = calc_month(secs)
-    remaining_sec_in_month = m_remainder * month
-    day_from_remainder = remaining_sec_in_month / day
-    a_day = round(day_from_remainder, 4)
-    a_day_remainder = day_from_remainder % 1
-    return math.trunc(a_day), a_day_remainder
+    m = calc_month(secs)
+    rem_secs = remain_secs_in_current_year(secs)
+    if leap_yr(calc_year(secs)):
+        counter = 0
+        while counter <= m:
+            num_days = LEAP_MONTH_DAYS[counter]
+            t_secs = num_days * day
+            if rem_secs - t_secs < 0:
+                return math.floor(rem_secs / day)
+            else:
+                rem_secs -= t_secs
+                counter += 1
+    else:
+        counter = 0
+        while counter <= m:
+            num_days = REG_MONTH_DAYS[counter]
+            t_secs = num_days * day
+            if rem_secs - t_secs < 0:
+                return math.floor(rem_secs / day)
+            else:
+                rem_secs -= t_secs
+                counter += 1
 
 
 def my_datetime(num_sec):
-    get_calc_year, _ = calc_year(num_sec)
-    get_calc_month, _ = calc_month(num_sec)
-    get_calc_day, _ = calc_day(num_sec)
+    get_calc_year = calc_year(num_sec)
+    get_calc_month = calc_month(num_sec)
+    get_calc_day = calc_day(num_sec)
 
     current_month = epoc_month + get_calc_month
     current_day = epoc_day + get_calc_day
     current_year = epoc_year + get_calc_year
     return str(current_month).zfill(2) + '-' + str(current_day).zfill(
         2) + '-' + str(current_year)
+
+
+if __name__ == "__main__":
+    # leap year 2188
+    # print(f"My day: {my_datetime(6884312400)}")
+    # print(datetime.utcfromtimestamp(6884312400).strftime('%m-%d-%Y'))
+
+    # 03-01-2100
+    print(f"My day: {my_datetime(4107567600)}")
+    print(datetime.utcfromtimestamp(4107567600).strftime('%m-%d-%Y'))
+    print(f"My day: {my_datetime(4107481200)}")
+    print(datetime.utcfromtimestamp(4107481200).strftime('%m-%d-%Y'))
