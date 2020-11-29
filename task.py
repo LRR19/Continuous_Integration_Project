0a855c345053c86b644b663fb4a62234285bd9c2
leyden recinos <leyden.recinos@gmail.com>
2020-11-28 23:59:09 -0800

    fixed all edge cases i can think of.

--- task.py
+++ task.py
@@ -16,57 +16,76 @@ def leap_yr(lp_year):
     return lp_year % 4 == 0 and (lp_year % 100 != 0 or lp_year % 400 == 0)
 
 
-def calc_year(secs) -> int:
+def calc_year2(secs) -> int:
     """Converts seconds to a year"""
     current_year_in_sec = 0
     while True:
         if leap_yr(math.floor(current_year_in_sec / year) + epoc_year):
+            # if adding another year with leap day doesn't overflow our
+            # given seconds then we can add a new year and day
             if current_year_in_sec + year + day <= secs:
                 current_year_in_sec += year + day
             else:
                 break
         else:
+            # if adding another year doesn't overflow our given seconds
+            # then we can add a new year
             if current_year_in_sec + year <= secs:
                 current_year_in_sec += year
             else:
                 break
 
-    # if leap_yr(math.floor(current_year_in_sec / year) + epoc_year):
-    #     return math.floor(current_year_in_sec / (year + day))
-    # else:
+    return math.floor(current_year_in_sec / year)
+
+
+def calc_year(secs) -> int:
+    """Converts seconds to a year"""
+    current_year_in_sec = 0
+    while True:
+        if current_year_in_sec + year <= secs:
+            current_year_in_sec += year
+        else:
+            break
+
     return math.floor(current_year_in_sec / year)
 
 
 def calc_month(secs) -> int:
     """Converts seconds to a month"""
-    y = calc_year(secs)
+    y = calc_year2(secs)
     rem_secs = remain_secs_in_current_year(secs)
-    m = 0
+    calculated_month = 0
     if leap_yr(y + epoc_year):
-        for v in LEAP_MONTH_DAYS:
-            sec_in_month = v * day
+        month_itr = 0
+        while True:
+            if month_itr > 11:
+                month_itr = 0
+            sec_in_month = LEAP_MONTH_DAYS[month_itr] * day
             if rem_secs - sec_in_month < 0:
-                if abs(rem_secs - sec_in_month) == day:  # midnight 12:00am
-                    m += 1
+                if abs(rem_secs - sec_in_month) <= day:  # midnight 12:00am
+                    calculated_month += 1
                 break
             else:
                 rem_secs -= sec_in_month
-                m += 1
+                calculated_month += 1
+                month_itr += 1
     else:
-        for v in REG_MONTH_DAYS:
-            sec_in_month = v * day
+        month_itr = 0
+        while True:
+            if month_itr > 11:
+                month_itr = 0
+            sec_in_month = REG_MONTH_DAYS[month_itr] * day
             if rem_secs - sec_in_month < 0:
-                if abs(rem_secs - sec_in_month) == day:  # midnight 12:00am
-                    m += 1
                 break
             else:
                 rem_secs -= sec_in_month
-                m += 1
-    return m
+                calculated_month += 1
+                month_itr += 1
+    return calculated_month
 
 
 def remain_secs_in_current_year(secs) -> int:
-    i = calc_year(secs)
+    i = calc_year2(secs)
     leap_month_years = 0
     reg_month_years = 0
     while i > 0:
@@ -88,47 +107,63 @@ def calc_day(secs) -> int:
     """Converts seconds to a day"""
     m = calc_month(secs)
     rem_secs = remain_secs_in_current_year(secs)
-    if leap_yr(calc_year(secs) + epoc_year):
+    if leap_yr(calc_year2(secs) + epoc_year):
         counter = 0
+        internal_counter = 0
         while counter <= m:
-            num_days = LEAP_MONTH_DAYS[counter]
+            if counter > 11 and internal_counter > 11:
+                internal_counter = 0
+            num_days = LEAP_MONTH_DAYS[internal_counter]
             total_sec_in_month = num_days * day
-            if rem_secs - total_sec_in_month < 0:
-                if abs(rem_secs - total_sec_in_month) == day:  # midnight 12:00am
-                    return 0
-                elif abs(rem_secs - total_sec_in_month) == day + day:  # leap day @ midnight 12:00am
+            if math.ceil((total_sec_in_month - rem_secs) / day) == \
+                    LEAP_MONTH_DAYS[internal_counter] - 1:
+                return 0
+            elif math.ceil((total_sec_in_month - rem_secs) / day) >= \
+                    LEAP_MONTH_DAYS[internal_counter]:
+                return 0
+            elif math.ceil((total_sec_in_month - rem_secs) / day) == 1:
+                return 0
+            elif rem_secs - total_sec_in_month < 0:
+                if counter > 11:
+                    return math.ceil(rem_secs / day) + 1
+                else:
                     return math.floor(rem_secs / day) + 1
-                return math.floor(rem_secs / day)
             else:
                 rem_secs -= total_sec_in_month
                 counter += 1
+                internal_counter += 1
     else:
         counter = 0
+        internal_counter = 0
         while counter <= m:
-            num_days = REG_MONTH_DAYS[counter]
+            if counter > 11 and internal_counter > 11:
+                internal_counter = 0
+            num_days = REG_MONTH_DAYS[internal_counter]
             total_sec_in_month = num_days * day
             if rem_secs - total_sec_in_month < 0:
-                if abs(rem_secs - total_sec_in_month) == day:  # midnight 12:00am
-                    return 0
-                return math.floor(rem_secs / day)
+                return REG_MONTH_DAYS[internal_counter] - \
+                       math.ceil((total_sec_in_month - rem_secs) / day)
             else:
                 rem_secs -= total_sec_in_month
                 counter += 1
+                internal_counter += 1
 
     return math.floor(rem_secs / day)
-    # rem_secs /= day
-    # if 0 < rem_secs < 1:
-    #     rem_secs = 1
-    # else:
-    #     rem_secs = math.floor(rem_secs)
-    #
-    # return rem_secs
 
 
 def my_datetime(num_sec):
-    get_calc_year = calc_year(num_sec)
+    get_calc_year = calc_year2(num_sec)
     get_calc_month = calc_month(num_sec)
     get_calc_day = calc_day(num_sec)
+    # Handles case when 12/32/2000 to be 01/nn/2001
+    if get_calc_month + epoc_month == 12:
+        if get_calc_day + epoc_day > REG_MONTH_DAYS[11]:
+            get_calc_month = 0
+            get_calc_day = get_calc_day - REG_MONTH_DAYS[11]
+            get_calc_year += 1
+    elif get_calc_month + epoc_month > 12:
+        get_calc_year += 1
+        get_calc_month = get_calc_month - 12
 
     current_month = epoc_month + get_calc_month
     current_day = epoc_day + get_calc_day
@@ -138,12 +173,49 @@ def my_datetime(num_sec):
 
 
 if __name__ == "__main__":
-    # leap year 2188
+    # print(f"My day: {my_datetime(131634486250)}")
+    # print(datetime.utcfromtimestamp(131634486250).strftime('%m-%d-%Y'))
+    #
+    #
+    # print(f"My day: {my_datetime(54351580109)}")
+    # print(datetime.utcfromtimestamp(54351580109).strftime('%m-%d-%Y'))
+    # print(f"My day: {my_datetime(109904972918)}")
+    # print(datetime.utcfromtimestamp(109904972918).strftime('%m-%d-%Y'))
+    #
+    # print(f"My day: {my_datetime(14642439067)}")
+    # print(datetime.utcfromtimestamp(14642439067).strftime('%m-%d-%Y'))
+    # print(f"My day: {my_datetime(15557568132)}")
+    # print(datetime.utcfromtimestamp(15557568132).strftime('%m-%d-%Y'))
+    # print(f"My day: {my_datetime(136925670091)}")
+    # print(datetime.utcfromtimestamp(136925670091).strftime('%m-%d-%Y'))
+    #
+    #
+    # print(f"My day: {my_datetime(21521869118)}")
+    # print(datetime.utcfromtimestamp(21521869118).strftime('%m-%d-%Y'))
+    # print(f"My day: {my_datetime(138884725212)}")
+    # print(datetime.utcfromtimestamp(138884725212).strftime('%m-%d-%Y'))
+    # print(f"My day: {my_datetime(171519770657)}")
+    # print(datetime.utcfromtimestamp(171519770657).strftime('%m-%d-%Y'))
+    # print(f"My day: {my_datetime(114817389093)}")
+    # print(datetime.utcfromtimestamp(114817389093).strftime('%m-%d-%Y'))
+    # print(f"My day: {my_datetime(163943502679)}")
+    # print(datetime.utcfromtimestamp(163943502679).strftime('%m-%d-%Y'))
+    # print(f"My day: {my_datetime(148287045670)}")
+    # print(datetime.utcfromtimestamp(148287045670).strftime('%m-%d-%Y'))
+    print(f"My day: {my_datetime(233716089025)}")
+    print(datetime.utcfromtimestamp(233716089025).strftime('%m-%d-%Y'))
+    # print(f"My: {my_datetime(234951368048)}")
+    # print(datetime.utcfromtimestamp(234951368048).strftime('%m-%d-%Y'))
+    # print(f"My: {my_datetime(123176718521)}")
+    # print(datetime.utcfromtimestamp(123176718521).strftime('%m-%d-%Y'))
+    #
+    #
+    # # leap year 2188
     # print(f"My day: {my_datetime(6884312400)}")
     # print(datetime.utcfromtimestamp(6884312400).strftime('%m-%d-%Y'))
-
-    # 03-01-2100
-    print(f"My day: {my_datetime(4107567600)}")
-    print(datetime.utcfromtimestamp(4107567600).strftime('%m-%d-%Y'))
-    print(f"My day: {my_datetime(4107481200)}")
-    print(datetime.utcfromtimestamp(4107481200).strftime('%m-%d-%Y'))
+    #
+    # # 03-01-2100
+    # print(f"My day: {my_datetime(4107567600)}")
+    # print(datetime.utcfromtimestamp(4107567600).strftime('%m-%d-%Y'))
+    # print(f"My day: {my_datetime(4107481200)}")
+    # print(datetime.utcfromtimestamp(4107481200).strftime('%m-%d-%Y'))
