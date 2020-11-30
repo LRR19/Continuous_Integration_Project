aa766c2606e5c97f23a5aaac356642fc76e7696b
Abraham Cheng <31293817+aXpress@users.noreply.github.com>
2020-11-29 21:04:19 -0800

    Resolved conflicts

--- task.py
+++ task.py
@@ -1,57 +1,281 @@
 import math
 
-hour = 3600
-day = 86400
-week = 604800
-month = 2629743
-year = 31556926
-epoc_year = 1970
-epoc_month = 1
-epoc_day = 1
+YEAR = 31556926
+MONTH = YEAR / 12
+DAY = 86400
+EPOCH_YEAR = 1970
+EPOCH_MONTH = 1
+EPOCH_DAY = 1
+LEAP_MONTH_DAYS = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+REG_MONTH_DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+valid_hex_num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                 'A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f']
 
 
+# PART 1: Convert Hex to Decimal
+def conv_num(num_str):
+    hex_to_int = 0
+    pwr = 0
+    if invalid_hex_string(num_str):
+        return None
+    if num_str.startswith('.') or num_str.endswith('.'):
+        return format_float(num_str)
+    if pos_hex_num(num_str) is False and neg_hex_num(num_str) is False:
+        return num_str
+    # For loop to determine value of hex
+    for digit in reversed(num_str):
+        if valid_hex_digit(digit):
+            hex_to_int += convert_lower(digit) * 16 ** pwr
+            pwr += 1
+        if digit == 'x' or digit == '-':
+            continue
+        if not valid_hex_digit(digit):
+            return None
+    if pos_hex_num(num_str):
+        return hex_to_int
+    if neg_hex_num(num_str):
+        return hex_to_int * -1
+    return None
+
+
+# Helper function to validate hex string
+def invalid_hex_string(hex_str):
+    if count_period(hex_str) > 1 or hex_str == '':
+        return True
+    if pos_hex_num(hex_str) is False and neg_hex_num(hex_str) is False:
+        for hex_digit in hex_str:
+            if valid_hex_digit(hex_digit):
+                if valid_hex_num.index(hex_digit) > 9:
+                    return True
+    count_x = 0
+    count_neg = 0
+    for digit in hex_str:
+        if digit == 'x':
+            count_x += 1
+        if digit == '-':
+            count_neg += 1
+    if count_x > 1 or count_neg > 1:
+        return True
+    return False
+
+
+# Helper function that returns the number of periods in given string
+def count_period(hex_str):
+    count = 0
+    for char in hex_str:
+        if char == '.':
+            count += 1
+    return count
+
+
+# Helper function that checks for valid hex number
+def valid_hex_digit(hex_digit):
+    if hex_digit in valid_hex_num:
+        return True
+    return False
+
+
+# Helper function that returns true if hex value is positive
+def pos_hex_num(hex_str):
+    for digit in hex_str:
+        if digit == '-':
+            return False
+    if hex_str.startswith('0x'):
+        return True
+    return False
+
+
+# Helper function returns true if negative hex value
+def neg_hex_num(hex_str):
+    if hex_str.startswith('-0x'):
+        return True
+    return False
+
+
+# Helper function that inserts missing 0 next to period
+def format_float(hex_str):
+    if hex_str.startswith('.'):
+        return '0' + hex_str
+    elif hex_str.endswith('.'):
+        return hex_str + '0'
+    return hex_str
+
+
+# Helper function that converts lowercase hex digit to correct value
+def convert_lower(hex_num):
+    hex_index = valid_hex_num.index(hex_num)
+    if hex_index > 15:
+        return hex_index - 6
+    return hex_index
+
+
+# PART 2: Datetime
 def leap_yr(lp_year):
-    """Helper func: Returns true if it's a leap year and False if it's not"""
-    return lp_year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+    """Helper func: Returns true if it's a leap YEAR and False if it's not"""
+    return lp_year % 4 == 0 and (lp_year % 100 != 0 or lp_year % 400 == 0)
 
 
-def calc_year(secs) -> (int, float):
-    """Converts seconds to a year"""
-    some_year = secs / year
-    # take the number before the decimal
-    a_year = some_year // 1
-    a_year_remainder = some_year % 1
-    return math.trunc(a_year), a_year_remainder
+def calc_year(secs) -> int:
+    """Helper func: Calculates the YEAR given the seconds"""
+    current_year_in_sec = 0
+    while True:
+        if leap_yr(math.floor(current_year_in_sec / YEAR) + EPOCH_YEAR):
+            # if adding another YEAR with leap DAY doesn't overflow our
+            # given seconds then we can add a new YEAR and DAY
+            if current_year_in_sec + YEAR + DAY <= secs:
+                current_year_in_sec += YEAR + DAY
+            else:
+                break
+        else:
+            # if adding another YEAR doesn't overflow our given seconds
+            # then we can add a new YEAR
+            if current_year_in_sec + YEAR <= secs:
+                current_year_in_sec += YEAR
+            else:
+                break
+
+    return math.floor(current_year_in_sec / YEAR)
 
 
-def calc_month(secs) -> (int, float):
-    """Converts seconds to a month"""
-    y, y_remainder = calc_year(secs)
-    remaining_sec_in_year = y_remainder * year
-    month_from_remainder = remaining_sec_in_year / month
-    a_month = month_from_remainder // 1
-    a_month_remainder = month_from_remainder % 1
-    return math.trunc(a_month), a_month_remainder
+def calc_month(secs) -> int:
+    """Helper func: Calculates MONTH of the year given the remaining seconds"""
+    y = calc_year(secs)
+    rem_secs = remain_secs_in_current_year(secs)
+    calculated_month = 0
+    # if a leap year, iterate through the list of LEAP_MONTHS_DAYS to
+    # calculate the days in a month. If there is an overflow of days
+    # within a month, it means you go to the next month. If it's not a leap
+    # year then repeat same process using REG_MONTHS_DAYS.
+    if leap_yr(y + EPOCH_YEAR):
+        month_itr = 0
+        while True:
+            if month_itr > 11:
+                month_itr = 0
+            sec_in_month = LEAP_MONTH_DAYS[month_itr] * DAY
+            if rem_secs - sec_in_month < 0:
+                if abs(rem_secs - sec_in_month) <= DAY:  # midnight 12:00am
+                    calculated_month += 1
+                break
+            else:
+                rem_secs -= sec_in_month
+                calculated_month += 1
+                month_itr += 1
+    else:
+        month_itr = 0
+        while True:
+            if month_itr > 11:
+                month_itr = 0
+            sec_in_month = REG_MONTH_DAYS[month_itr] * DAY
+            if rem_secs - sec_in_month < 0:
+                break
+            else:
+                rem_secs -= sec_in_month
+                calculated_month += 1
+                month_itr += 1
+    return calculated_month
 
 
-def calc_day(secs) -> (int, float):
-    """Converts seconds to a day"""
-    m, m_remainder = calc_month(secs)
-    remaining_sec_in_month = m_remainder * month
-    day_from_remainder = remaining_sec_in_month / day
-    a_day = round(day_from_remainder, 4)
-    a_day_remainder = day_from_remainder % 1
-    return math.trunc(a_day), a_day_remainder
+def remain_secs_in_current_year(secs) -> int:
+    """Helper func: Calculates the remaining seconds from given year"""
+    i = calc_year(secs)
+    leap_month_years = 0
+    reg_month_years = 0
+    # Calculates the remaining seconds(seconds that are left over after the
+    # year has been calculated) for a leap and regular year.
+    while i > 0:
+        if leap_yr(i + EPOCH_YEAR):
+            leap_month_years += 1
+        else:
+            reg_month_years += 1
+        i -= 1
+
+    rem = secs - (((reg_month_years * 365) + (leap_month_years * 366)) *
+                  DAY)
+    if rem < 0:
+        rem = abs(rem)
+
+    return rem
+
+
+def calc_day(secs) -> int:
+    """Helper func: Calculates the day given the MONTH for a leap and
+       regular year"""
+    m = calc_month(secs)
+    rem_secs = remain_secs_in_current_year(secs)
+    if leap_yr(calc_year(secs) + EPOCH_YEAR):
+        return calc_day_leap(m, rem_secs)
+    else:
+        return calc_day_comm(m, rem_secs)
+
+
+def calc_day_leap(m, rem_secs) -> int:
+    """Helper func: Determines the DAY of the MONTH in a leap year"""
+    counter = 0
+    internal_counter = 0
+    while counter <= m:
+        if counter > 11 and internal_counter > 11:
+            internal_counter = 0
+        num_days = LEAP_MONTH_DAYS[internal_counter]
+        total_sec_in_month = num_days * DAY
+        if math.ceil((total_sec_in_month - rem_secs) / DAY) == \
+                LEAP_MONTH_DAYS[internal_counter] - 1:
+            return 0
+        elif math.ceil((total_sec_in_month - rem_secs) / DAY) >= \
+                LEAP_MONTH_DAYS[internal_counter]:
+            return 0
+        elif math.ceil((total_sec_in_month - rem_secs) / DAY) == 1:
+            return 0
+        elif rem_secs - total_sec_in_month < 0:
+            if counter > 11:
+                return math.ceil(rem_secs / DAY) + 1
+            else:
+                return math.floor(rem_secs / DAY) + 1
+        else:
+            rem_secs -= total_sec_in_month
+            counter += 1
+            internal_counter += 1
+
+
+def calc_day_comm(m, rem_secs) -> int:
+    """Helper func: Determines the DAY of the MONTH in a regular year"""
+    counter = 0
+    internal_counter = 0
+    while counter <= m:
+        if counter > 11 and internal_counter > 11:
+            internal_counter = 0
+        num_days = REG_MONTH_DAYS[internal_counter]
+        total_sec_in_month = num_days * DAY
+        if rem_secs - total_sec_in_month < 0:
+            return REG_MONTH_DAYS[internal_counter] - \
+                   math.ceil((total_sec_in_month - rem_secs) / DAY)
+        else:
+            rem_secs -= total_sec_in_month
+            counter += 1
+            internal_counter += 1
 
 
 def my_datetime(num_sec):
-    get_calc_year, _ = calc_year(num_sec)
-    get_calc_month, _ = calc_month(num_sec)
-    get_calc_day, _ = calc_day(num_sec)
+    """ Main function that utilizes above helpers to convert seconds in
+    MM-DD-YYYY"""
+    get_calc_year = calc_year(num_sec)
+    get_calc_month = calc_month(num_sec)
+    get_calc_day = calc_day(num_sec)
 
-    current_month = epoc_month + get_calc_month
-    current_day = epoc_day + get_calc_day
-    current_year = epoc_year + get_calc_year
+    # Handles case when 12/32/2000 to be 01/nn/2001
+    if get_calc_month + EPOCH_MONTH == 12:
+        if get_calc_day + EPOCH_DAY > REG_MONTH_DAYS[11]:
+            get_calc_month = 0
+            get_calc_day = get_calc_day - REG_MONTH_DAYS[11]
+            get_calc_year += 1
+    elif get_calc_month + EPOCH_MONTH > 12:
+        get_calc_year += 1
+        get_calc_month = get_calc_month - 12
+
+    current_month = EPOCH_MONTH + get_calc_month
+    current_day = EPOCH_DAY + get_calc_day
+    current_year = EPOCH_YEAR + get_calc_year
+
     return str(current_month).zfill(2) + '-' + str(current_day).zfill(
         2) + '-' + str(current_year)
 
--- tests.py
+++ tests.py
@@ -1,23 +1,108 @@
 import unittest
-import random
-from task import leap_yr, my_datetime
-from task import conv_endian
+from datetime import datetime
+from task import leap_yr, my_datetime, conv_endian, count_period, \
+    valid_hex_digit, pos_hex_num, neg_hex_num, conv_num, format_float, \
+    invalid_hex_string
 
 
 class TestCase(unittest.TestCase):
-
     """Unit tests for function 2 - def my_datetime(num_sec)"""
+
     def test_leap_yr_func(self):
         self.assertTrue(leap_yr(2000))
 
-    def test_yr_zero(self):
-        self.assertEqual(my_datetime(0), "01-01-1970")
-
-    # Testing 9999 years: 11,969
-    def test_secs1(self):
+    # YEAR 11,969
+    def test_secs(self):
         self.assertTrue(my_datetime(315537963048))
 
-    # Unit Tests for Function 3
+    def test_epoch0(self):
+        self.assertEqual("01-01-1970", my_datetime(0))
+
+    def test_epoch1(self):
+        self.assertEqual("01-01-1970", my_datetime(86399))
+
+    def test1(self):
+        self.assertEqual("11-29-1973", my_datetime(123456789))
+
+    def test2(self):
+        self.assertEqual("12-22-2282", my_datetime(9876543210))
+
+    # 09-26-1988 @ 12:00:00 am
+    def test3(self):
+        self.assertEqual(
+            datetime.utcfromtimestamp(591235200).strftime('%m-%d-%Y'),
+            my_datetime(591235200))
+
+    # 07-01-2707. Beginning of the MONTH
+    def test4(self):
+        self.assertEqual(
+            datetime.utcfromtimestamp(23273096400).strftime('%m-%d-%Y'),
+            my_datetime(23273096400))
+
+    # 07-31-2185. End of the MONTH
+    def test5(self):
+        self.assertEqual(
+            datetime.utcfromtimestamp(6803096400).strftime('%m-%d-%Y'),
+            my_datetime(6803096400))
+
+    # 01-01-3131. Beginning of the YEAR
+    def test6(self):
+        self.assertEqual(
+            datetime.utcfromtimestamp(36637621200).strftime('%m-%d-%Y'),
+            my_datetime(36637621200))
+
+    # 12-31-9999. End of the YEAR
+    def test7(self):
+        self.assertEqual(
+            datetime.utcfromtimestamp(253402261200).strftime('%m-%d-%Y'),
+            my_datetime(253402261200))
+
+    # 01-01-2201. Beginning of a non-leap YEAR
+    def test8(self):
+        self.assertEqual(
+            datetime.utcfromtimestamp(7289738124).strftime('%m-%d-%Y'),
+            my_datetime(7289738124))
+
+    # 11-30-2007. random DAY
+    def test9(self):
+        self.assertEqual(
+            datetime.utcfromtimestamp(1196402400).strftime('%m-%d-%Y'),
+            my_datetime(1196402400))
+
+    # 12-22-6065 @ 11:59:59 pm
+    def test10(self):
+        self.assertEqual(
+            datetime.utcfromtimestamp(129256559999).strftime('%m-%d-%Y'),
+            my_datetime(129256559999))
+
+    # 12-31-8573 @ 12:00:00 am
+    def test11(self):
+        self.assertEqual(
+            datetime.utcfromtimestamp(208402070400).strftime('%m-%d-%Y'),
+            my_datetime(208402070400))
+
+    # LEAP: 02-29-1972
+    def test12(self):
+        self.assertEqual(
+            datetime.utcfromtimestamp(68169600).strftime('%m-%d-%Y'),
+            my_datetime(68169600))
+
+    # LEAP: 02-29-1972 @ 11:59:59 pm
+    def test13(self):
+        self.assertEqual(datetime.utcfromtimestamp(68255999).strftime(
+            '%m-%d-%Y'), my_datetime(68255999))
+
+    # LEAP: 02-29-2024 @ 12:00:00 am
+    def test14(self):
+        self.assertEqual(datetime.utcfromtimestamp(1709164800).strftime(
+            '%m-%d-%Y'), my_datetime(1709164800))
+
+    # LEAP: 03-01-2020. Beginning of the MONTH after Feb (leap)
+    def test15(self):
+        self.assertEqual(datetime.utcfromtimestamp(1583042400).strftime(
+            '%m-%d-%Y'), my_datetime(1583042400))
+
+    """Unit tests for function 3 - def int_to_hex(num)"""
     def test_big(self):
         self.assertEqual(conv_endian(954786), "0E 91 A2")
 
@@ -58,6 +143,28 @@ class TestCase(unittest.TestCase):
             little_formatted = ' '.join(format(x, '02x') for x in t).upper()
             self.assertEqual(conv_endian(random_int, 'little'), little_formatted)
 
+
+    """Unit tests for function 1 - def conv_num(num_str)"""
+    def test_count_period(self):
+        self.assertEqual(count_period('abc....def'), 4)
+
+    def test_valid_hex_digit(self):
+        self.assertTrue(valid_hex_digit('a'))
+
+    def test_pos_hex_num(self):
+        self.assertTrue(pos_hex_num('0xA'))
+
+    def test_neg_hex_num(self):
+        self.assertTrue(neg_hex_num('-0xA'))
+
+    def test_conv_num(self):
+        self.assertEqual(conv_num('0x1A'), 26)
+
+    def test_format_float(self):
+        self.assertEqual(format_float('.123'), '0.123')
+
+    def test_invalid_hex_string(self):
+        self.assertEqual(invalid_hex_string('12345A'), True)
 
 if __name__ == '__main__':
     unittest.main()
